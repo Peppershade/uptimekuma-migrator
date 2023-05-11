6c5536e402cdc95fa8662aca40f7d7146decc757
Peppershade <admin@peppershade.nl>
2023-05-11 16:41:58 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# uptimekuma-migrator
- Simle migrator to UptimeKuma from Uptimerobot
+# Uptime Kuma Monitor Importer
+
+This script allows you to sync/import monitors from UptimeRobot to Uptime Kuma using the Uptime Kuma API.
+
+## Prerequisites
+
+Before running the script, make sure you have the following information:
+
+- UptimeRobot API key: Obtain this from your UptimeRobot account.
+- Uptime Kuma credentials: You need the protocol (http or https), URL or IP address of your Uptime Kuma instance, username, and password.
+
+## Installation
+
+1. Clone this repository or download the script to your local machine.
+
+2. Install the required dependencies by running the following command:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+
+3. Update the following variables in the script:
+
+- `uptimerobot_api_key`: Replace `'UPTIMEROBOT-API-KEY'` with your UptimeRobot API key.
+- `uptimekuma_protocol`: Set this to `'http'` or `'https'` depending on the protocol used by your Uptime Kuma instance.
+- `uptimekuma_url`: Replace `'127.0.0.1:3001'` with the domain or IP address of your Uptime Kuma instance.
+- `uptimekuma_username`: Replace `'admin'` with your Uptime Kuma username.
+- `uptimekuma_password`: Replace `'password'` with your Uptime Kuma password.
+- `skip_paused_monitors`: Set this to `True` if you want to skip syncing paused monitors.
+- `start_clean`: Set this to `True` if you want to clean all existing monitors from Uptime Kuma before syncing.
+
+## Usage
+
+To run the script, execute the following command:
+
+```python sync.py```
+
+
+The script will perform the following steps:
+
+1. Fetch monitors from UptimeRobot API.
+2. Sync each monitor to Uptime Kuma API.
+3. Print status messages and errors for each monitor sync.
+
+Please note that the script will print verbose output during the process.
+
+## Notes
+
+- The script uses the `requests` library to make API requests, so make sure it is installed before running the script.
+- The `uptime_kuma_api` module is required for interacting with the Uptime Kuma API. Make sure it is present in the same directory as the script.
+
+## License
+
+This script is released under the [MIT License](LICENSE).
+
---
+++ migrator.py
@@ -0,0 +1,173 @@
+import requests
+import json
+from uptime_kuma_api import UptimeKumaApi
+from uptime_kuma_api import UptimeKumaException
+from uptime_kuma_api import MonitorType
+
+# Your details
+uptimerobot_api_key = 'UPTIMEROBOT-API-KEY'  # your UptimeRobot API key
+uptimekuma_protocol = 'https'  # http or https
+uptimekuma_url = '127.0.0.1:3001'  # domain or IP address of your Uptime Kuma instance
+uptimekuma_username = 'admin'  # your Uptime Kuma username
+uptimekuma_password = 'password'  # your Uptime Kuma password
+
+# Options
+skip_paused_monitors = False  # skip paused monitors
+start_clean = False  # clean all monitors from Uptime Kuma before syncing
+
+# Uptime Kuma API login
+api = UptimeKumaApi(f'{uptimekuma_protocol}://{uptimekuma_url}')
+api.login(uptimekuma_username, uptimekuma_password)
+
+
+# Clean monitors from Uptime Kuma API
+def clean_uptimekuma_monitors():
+    """
+    Cleans monitors from Uptime Kuma API.
+    """
+    print('Cleaning monitors from Uptime Kuma API.')
+    monitors = api.get_monitors()
+    for monitor in monitors:
+        api.delete_monitor(monitor['id'])
+        print(f"Monitor '{monitor['name']}' deleted from Uptime Kuma.")
+    print('Done cleaning monitors from Uptime Kuma API.')
+
+
+# Fetch monitors from UptimeRobot API
+def fetch_uptimerobot_monitors():
+    """
+    Fetches monitor data from UptimeRobot API.
+    Returns a list of monitors.
+    """
+    url = "https://api.uptimerobot.com/v2/getMonitors"
+
+    payload = f"api_key={uptimerobot_api_key}&format=json&logs=1"
+    headers = {
+        'content-type': "application/x-www-form-urlencoded",
+        'cache-control': "no-cache"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    data = json.loads(response.text)
+    if data['stat'] == 'ok':
+        return data['monitors']
+    else:
+        raise Exception('Failed to fetch monitors from UptimeRobot API.')
+
+
+# Sync monitor to Uptime Kuma API
+def sync_monitor_to_uptimekuma(monitor):
+    """
+    Syncs a monitor to Uptime Kuma API.
+    """
+
+    # Check if monitor already exists in Uptime Kuma
+    if check_if_monitor_exists(monitor):
+        print(f"Monitor '{monitor['friendly_name']}' already exists in Uptime Kuma.")
+        return
+
+    # Skip paused monitors
+    if skip_paused_monitors and monitor['status'] == 0:
+        print(f"Monitor '{monitor['friendly_name']}' is paused and will be skipped.")
+        return
+
+    # Add PORT monitor
+    if monitor['type'] == 4:
+        try:
+            api.add_monitor(
+                type=MonitorType.PORT,
+                name=monitor['friendly_name'],
+                url=monitor['url'],
+                interval=monitor['interval'],
+                hostname=monitor['url'],
+                port=monitor['port'],
+            )
+            print(f"Monitor '{monitor['friendly_name']}' added to Uptime Kuma with type PORT.")
+        except UptimeKumaException as e:
+            print(f"Monitor '{monitor['friendly_name']}' failed to sync to Uptime Kuma.")
+            print(e)
+
+            return
+
+    # Add HTTP monitor
+    elif monitor['type'] == 1:
+        try:
+            api.add_monitor(
+                type=MonitorType.HTTP,
+                name=monitor['friendly_name'],
+                url=monitor['url'],
+                hostname=monitor['url'],
+                interval=monitor['interval'],
+            )
+            print(f"Monitor '{monitor['friendly_name']}' added to Uptime Kuma with type HTTP.")
+        except UptimeKumaException as e:
+            print(f"Monitor '{monitor['friendly_name']}' failed to sync to Uptime Kuma.")
+            print(e)
+
+    # Add KEYWORD monitor
+    elif monitor['type'] == 2:
+        try:
+            api.add_monitor(
+                type=MonitorType.KEYWORD,
+                name=monitor['friendly_name'],
+                url=monitor['url'],
+                hostname=monitor['url'],
+                interval=monitor['interval'],
+                keyword=monitor['keyword_type'],
+            )
+            print(f"Monitor '{monitor['friendly_name']}' added to Uptime Kuma with type KEYWORD.")
+        except UptimeKumaException as e:
+            print(f"Monitor '{monitor['friendly_name']}' failed to sync to Uptime Kuma.")
+            print(e)
+
+    # Add PING monitor
+    elif monitor['type'] == 3:
+        try:
+            api.add_monitor(
+                type=MonitorType.PING,
+                name=monitor['friendly_name'],
+                hostname=monitor['url'],
+                url=monitor['url'],
+                interval=monitor['interval'],
+            )
+            print(f"Monitor '{monitor['friendly_name']}' added to Uptime Kuma with type PING.")
+        except UptimeKumaException as e:
+            print(f"Monitor '{monitor['friendly_name']}' failed to sync to Uptime Kuma.")
+            print(e)
+
+    else:
+        print(f"Monitor '{monitor['friendly_name']}' has an unsupported type.")
+        return
+
+
+# Check if monitor already exists in Uptime Kuma
+def check_if_monitor_exists(monitor):
+    """
+    Checks if a monitor already exists in Uptime Kuma.
+    Returns True if monitor exists, False if not.
+    """
+    monitors = api.get_monitors()
+    for m in monitors:
+        if m['name'] == monitor['friendly_name']:
+            return True
+    return False
+
+
+# Clean monitors from Uptime Kuma API
+if start_clean:
+    clean_uptimekuma_monitors()
+    print('Done cleaning monitors from Uptime Kuma API.')
+
+# Fetch monitors from UptimeRobot API
+print('Fetching monitors from UptimeRobot API.')
+monitors = fetch_uptimerobot_monitors()
+print(f'Fetched {len(monitors)} monitors from UptimeRobot API.')
+
+# Sync each monitor to Uptime Kuma API
+print('Syncing monitors to Uptime Kuma API.')
+for monitor in monitors:
+    sync_monitor_to_uptimekuma(monitor)
+print('Done syncing monitors to Uptime Kuma API.')
+
+# Quit program
+print('Done!')
+quit()
